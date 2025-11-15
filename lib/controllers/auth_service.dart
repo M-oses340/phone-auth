b0a5b80d5d3905e32970a1e29a1e73ccb44385aa
Moses Omwa <mosesomwa7@gmail.com>
2025-11-15 15:38:14 +0300

    phone auth

--- lib/controllers/auth_service.dart
+++ lib/controllers/auth_service.dart
@@ -11,26 +11,19 @@ class AuthService {
     await _auth.verifyPhoneNumber(
       phoneNumber: phone,
       timeout: const Duration(seconds: 60),
-
       verificationCompleted: (PhoneAuthCredential credential) async {
-        // Auto-verification (Android only)
         try {
           await _auth.signInWithCredential(credential);
         } catch (_) {}
       },
-
       verificationFailed: (FirebaseAuthException e) {
         completer.completeError(e.message ?? "Verification Failed");
       },
-
       codeSent: (String verificationId, int? resendToken) {
         completer.complete(verificationId);
       },
-
       codeAutoRetrievalTimeout: (String verificationId) {
-        if (!completer.isCompleted) {
-          completer.complete(verificationId);
-        }
+        if (!completer.isCompleted) completer.complete(verificationId);
       },
     );
 
